5bf471767f5842152d8da5e90353830663ddaff5
DiegoGalantee <diegogalante07@gmail.com>
2025-05-26 19:17:22 +0000

    proyecto terminado con cronometro para que cambie cada 10 segundos, boton e inputs para cambiar alto y ancho libremente, boton para cambiar la carta en pantalla y cambio de carta al refrescar la pagina

--- src/app.js
+++ src/app.js
@@ -5,7 +5,55 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+ window.onload = function () {
+      console.log("Hello Rigo from the console!");
+
+      const suits = ['♥', '♦', '♣', '♠'];
+      const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+      function generateRandomCard() {
+        const suit = suits[Math.floor(Math.random() * suits.length)];
+        const value = values[Math.floor(Math.random() * values.length)];
+
+        const card = document.getElementById('card');
+        const topSuit = document.getElementById('top-suit');
+        const bottomSuit = document.getElementById('bottom-suit');
+        const cardValue = document.getElementById('card-value');
+
+        topSuit.textContent = suit;
+        bottomSuit.textContent = suit;
+        cardValue.textContent = value;
+
+        card.classList.remove('heart', 'diamond', 'club', 'spade');
+
+        switch (suit) {
+          case '♥': card.classList.add('heart'); break;
+          case '♦': card.classList.add('diamond'); break;
+          case '♣': card.classList.add('club'); break;
+          case '♠': card.classList.add('spade'); break;
+        }
+      }
+
+  function resetInterval() {
+    clearInterval(intervalId);
+    intervalId = setInterval(generateRandomCard, 10000)
+  }
+      
+  generateRandomCard();
+
+  let intervalId = setInterval(generateRandomCard, 10000)
+
+  
+  document.getElementById('randomBtn').addEventListener('click', () => {
+    generateRandomCard();
+    resetInterval();
+  });
+  document.getElementById('resizeBtn').addEventListener('click', () => {
+  const width = document.getElementById('cardWidth').value;
+  const height = document.getElementById('cardHeight').value;
+  const card = document.getElementById('card');
+
+  if (width) card.style.width = width + "px";
+  if (height) card.style.height = height + "px";
+});
+}
